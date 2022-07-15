27fe5cbdbe31f44f5b2b4f106495858e94bd24ed
Aum Thacker <AumThacker>
2022-07-16 02:02:11 +0530

    Negative numbers are handled.

--- src/stringcalculatorkata/StringCalculatorKata.java
+++ src/stringcalculatorkata/StringCalculatorKata.java
@@ -5,6 +5,8 @@
  */
 package stringcalculatorkata;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Aum Thacker
@@ -16,7 +18,7 @@ public class StringCalculatorKata {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    public int Add(String numbers) {
+    public int Add(String numbers) throws Exception {
         if (numbers.isEmpty()) {
             return 0;
         } else if (numbers.length() == 1) {
@@ -35,11 +37,25 @@ public class StringCalculatorKata {
         }
     }
     
-    public int getSum(String[] nums)
+    public int getSum(String[] nums) throws Exception
     {
+        ArrayList<Integer> neg_nums = new ArrayList<Integer>();
         int sum=0;
         for (String num : nums) {
-            sum += stringToInt(num);
+            if(Integer.parseInt(num) < 0){
+                neg_nums.add(Integer.parseInt(num));
+            }
+            else{
+                sum += stringToInt(num);
+            }
+        }
+        if(neg_nums.size() != 0){
+            String exc_msg = "negatives not allowed : ";
+            for(int i = 0;i < neg_nums.size();i++){
+                exc_msg += String.valueOf(neg_nums.get(i)) + " ";
+            }
+            System.out.println(exc_msg);
+            throw new Exception(exc_msg);
         }
         return sum;
     }
--- test/StringCalculatorKataTest.java
+++ test/StringCalculatorKataTest.java
@@ -7,6 +7,7 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import stringcalculatorkata.StringCalculatorKata;
 
 /**
@@ -27,32 +28,37 @@ public class StringCalculatorKataTest {
     }
 
     @Test
-    public void emptyString() {
+    public void emptyString() throws Exception {
         assertEquals(cal.Add(""), 0);
     }
 
     @Test
-    public void singleNumber() {
+    public void singleNumber() throws Exception {
         assertEquals(cal.Add("1"), 1);
     }
 
     @Test
-    public void twoNumbers() {
+    public void twoNumbers() throws Exception {
         assertEquals(cal.Add("1,2"), 3);
     }
     
     @Test
-    public void unknownNumbers() {
+    public void unknownNumbers() throws Exception {
         assertEquals(cal.Add("1,2,3,4"),10);
     }
     
     @Test
-    public void twoNumbersNewLine() {
+    public void twoNumbersNewLine() throws Exception {
         assertEquals(cal.Add("1\n2"),3);
     }
     
     @Test
-    public void customDelimiter() {
+    public void customDelimiter() throws Exception {
         assertEquals(cal.Add("//;\n1;2"),3);
+    }
+    
+    @Test(expected = Exception.class)
+    public void negativeNumber() throws Exception {
+        cal.Add("-1\n-2,3");
     }
 }
